50d222f7df5f5ba1143f02e1e4855f666d9aee48
Ali <sycanpvp@gmail.com>
2017-10-14 18:53:06 +0100

    Cleaned up code

--- src/main/java/net/legenda/DiscordBot/listeners/AntiSpam.java
+++ src/main/java/net/legenda/DiscordBot/listeners/AntiSpam.java
@@ -1,14 +1,10 @@
 package net.legenda.DiscordBot.listeners;
 
-import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.legenda.DiscordBot.command.Command;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,14 +15,10 @@ public class AntiSpam extends ListenerAdapter {
         List<Message> history = event.getTextChannel().getIterableHistory().complete().stream().limit(3).filter(msg -> !msg.equals(event.getMessage())).collect(Collectors.toList());
         int spam = history.stream().filter(message -> message.getAuthor().equals(event.getAuthor()) && !message.getAuthor().isBot()).filter(msg -> (event.getMessage().getCreationTime().toEpochSecond() - msg.getCreationTime().toEpochSecond()) < 2).collect(Collectors.toList()).size();
         if(spam == 1){
-            Command.sendErrorMessage("Please leave 2 seconds between messages, " + event.getAuthor().getAsMention(), event.getChannel());
+            Command.sendErrorMessage("Please leave 2 seconds between messages, " + event.getAuthor().getAsMention(), event.getChannel(), true);
             event.getMessage().delete().queue();
 
-        } else if (spam > 5) {
         }
     }
 
-    public static boolean isSpamming(Guild guild){
-        return false;
-    }
 }
