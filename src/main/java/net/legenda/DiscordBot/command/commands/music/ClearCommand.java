745d4ff60e01f32a9f1b6ae7a4940a7ffb9819ac
Ali <sycanpvp@gmail.com>
2017-10-20 16:19:40 +0100

    Clear Command

---
+++ src/main/java/net/legenda/DiscordBot/command/commands/music/ClearCommand.java
@@ -0,0 +1,15 @@
+package net.legenda.DiscordBot.command.commands.music;
+
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.legenda.DiscordBot.Main;
+import net.legenda.DiscordBot.command.Command;
+
+@Command.cmdInfo(name = "Clear", description = "Clears the current queue", type = Command.Type.Music)
+public class ClearCommand extends Command {
+
+    @Override
+    public void execute(String[] args, MessageReceivedEvent event) {
+        Main.INSTANCE.musicUtils.clear(event.getGuild());
+        sendEmbedMessage(":arrows_counterclockwise: Cleared Queue:", event.getTextChannel(), false);
+    }
+}
