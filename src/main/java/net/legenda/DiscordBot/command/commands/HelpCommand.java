8b9a80d722888bda46f30b44e57a7f3e471d532b
Ali <sycanpvp@gmail.com>
2017-10-20 15:39:24 +0100

    Added Music Commands

--- src/main/java/net/legenda/DiscordBot/command/commands/HelpCommand.java
+++ src/main/java/net/legenda/DiscordBot/command/commands/HelpCommand.java
@@ -2,6 +2,8 @@ package net.legenda.DiscordBot.command.commands;
 
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.legenda.DiscordBot.command.Command;
+import net.legenda.DiscordBot.exceptions.InvalidCommandArgumentException;
+import net.legenda.DiscordBot.exceptions.InvalidCommandStateException;
 import net.legenda.DiscordBot.managers.CommandManager;
 
 @Command.cmdInfo(name = "Help", description = "A help command")
@@ -10,17 +12,43 @@ public class HelpCommand extends Command {
     @Override
     public void execute(String[] args, MessageReceivedEvent event) {
         StringBuilder msg = new StringBuilder();
-        msg.append("*Admin Commands:* \n ");
-        CommandManager.commands.values().stream().filter(cmd -> cmd.getType().equals(Type.Admin)).forEach(cmd -> msg.append(cmd.getName() + ": " + cmd.getDescription() + "\n "));
+        if(args.length < 1){
+            msg.append("__Admin Commands:__ \n");
+            CommandManager.commands.values().stream().filter(cmd -> cmd.getType().equals(Type.Admin)).forEach(cmd -> msg.append(cmd.getName() + ": " + cmd.getDescription() + "\n"));
 
-        msg.append("\n*Fun Commands:*\n ");
-        CommandManager.commands.values().stream().filter(cmd -> cmd.getType().equals(Type.Fun)).forEach(cmd -> msg.append(cmd.getName() + ": " + cmd.getDescription() + "\n "));
+            msg.append("\n__Music Commands:__ \n");
+            CommandManager.commands.values().stream().filter(cmd -> cmd.getType().equals(Type.Music)).forEach(cmd -> msg.append(cmd.getName() + ": " + cmd.getDescription() + "\n"));
 
-        msg.append("\n*Other Commands:*\n ");
-        CommandManager.commands.values().stream().filter(cmd -> cmd.getType().equals(Type.Misc)).forEach(cmd -> msg.append(cmd.getName() + ": " + cmd.getDescription() + "\n "));
+            msg.append("\n__Fun Commands:__\n");
+            CommandManager.commands.values().stream().filter(cmd -> cmd.getType().equals(Type.Fun)).forEach(cmd -> msg.append(cmd.getName() + ": " + cmd.getDescription() + "\n"));
+
+            msg.append("\n__Other Commands:__\n");
+            CommandManager.commands.values().stream().filter(cmd -> cmd.getType().equals(Type.Misc)).forEach(cmd -> msg.append(cmd.getName() + ": " + cmd.getDescription() + "\n"));
+        } else{
+            String casez = args[0];
+            Type type = null;
+            switch(casez.toLowerCase()){
+                case "admin":
+                    type = Type.Admin;
+                    break;
+                case "music":
+                    type = Type.Music;
+                    break;
+                case "fun" :
+                    type = Type.Fun;
+                    break;
+                case "misc":
+                    type = Type.Misc;
+            }
+            if(type == null)
+                throw new InvalidCommandArgumentException("Usage: .help <cmdType>");
+            final Type type2 = type;
+            msg.append("__" + type2.name()+ " Commands__:\n\n");
+            CommandManager.commands.values().stream().filter(cmd -> cmd.getType().equals(type2)).forEach(cmd -> msg.append(cmd.getName() + ": " + cmd.getDescription() + "\n"));
+        }
 
         String message = new String(msg);
-        sendEmbedMessage(message, event.getChannel(), false);
+        sendEmbedMessage(message, event.getTextChannel(), false);
 
     }
 }
