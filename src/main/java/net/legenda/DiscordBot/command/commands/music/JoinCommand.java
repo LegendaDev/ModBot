dcebfec8d825223a59dbf3b238ea6b1d32b83a7a
Ali <sycanpvp@gmail.com>
2017-10-20 17:14:00 +0100

    Added Join/Connect Command

---
+++ src/main/java/net/legenda/DiscordBot/command/commands/music/JoinCommand.java
@@ -0,0 +1,18 @@
+package net.legenda.DiscordBot.command.commands.music;
+
+import net.dv8tion.jda.core.entities.VoiceChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.legenda.DiscordBot.command.Command;
+import net.legenda.DiscordBot.exceptions.InvalidCommandStateException;
+
+@Command.cmdInfo(name = "Clear", description = "Clears the current queue", type = Command.Type.Music, alias = {"connect"})
+public class JoinCommand extends Command {
+
+    @Override
+    public void execute(String[] args, MessageReceivedEvent event) {
+        VoiceChannel channel = event.getMember().getVoiceState().getChannel();
+        if(channel == null)
+            throw new InvalidCommandStateException("You must be in a VoiceChannel to summon the bot");
+        event.getGuild().getAudioManager().openAudioConnection(channel);
+    }
+}
