d4b2ca69c183bd6ca242212cad0307d88ee467e5
Ali <sycanpvp@gmail.com>
2017-10-20 16:34:55 +0100

    Added Shuffle Command

--- src/main/java/net/legenda/DiscordBot/command/commands/music/ShuffleCommand.java
+++ src/main/java/net/legenda/DiscordBot/command/commands/music/ShuffleCommand.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.legenda.DiscordBot.Main;
 import net.legenda.DiscordBot.command.Command;
 
-@Command.cmdInfo(name = "Clear", description = "Clears the current queue", type = Command.Type.Music)
+@Command.cmdInfo(name = "Shuffle", description = "Shuffles the queue", type = Command.Type.Music)
 public class ShuffleCommand extends Command {
 
     @Override
