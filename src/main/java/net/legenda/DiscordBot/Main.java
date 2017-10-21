d16c7e839b77596d3a7ae4b409518ebe2193fc94
Ali <sycanpvp@gmail.com>
2017-10-22 00:20:14 +0100

    Improved

--- src/main/java/net/legenda/DiscordBot/Main.java
+++ src/main/java/net/legenda/DiscordBot/Main.java
@@ -9,29 +9,30 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.OnlineStatus;
 import net.dv8tion.jda.core.entities.Game;
-import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.legenda.DiscordBot.utils.MessageUtils;
+import net.legenda.DiscordBot.utils.MusicUtils;
 
 import javax.security.auth.login.LoginException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 
 public class Main {
 
     public static final Main INSTANCE = new Main();
 
-    private static JDA jdaBot;
+    public static JDA jdaBot;
     public String cmdPrefix = ".";
     public MessageUtils msgUtil = new MessageUtils();
+    public MusicUtils musicUtils = new MusicUtils();
     public CommandManager cmdManager = new CommandManager();
 
     public static HashMap<String, Role> roles;
     private static final SimpleLog LOGGER = SimpleLog.getLog(Main.class);
 
     public static void main(String[] args) throws LoginException, InterruptedException, RateLimitedException {
+        //bot Builder
         JDABuilder builder = new JDABuilder(AccountType.BOT);
         builder.setToken("MzQ4NDY0MzA1MTY0OTEwNjA1.DHnYmg.j3p3NPbxKTTcFOAF_gmyo01t_NI");
         builder.setStatus(OnlineStatus.ONLINE);
@@ -39,22 +40,25 @@ public class Main {
         builder.setAudioEnabled(true);
         builder.setGame(Game.of(".help"));
 
+        //initialise bot
         jdaBot = builder.buildBlocking();
+
+        //discord event listeners
         jdaBot.addEventListener(new MessageManager());
         jdaBot.addEventListener(new AntiSpam());
 
+        //get guild roles
         getRoles();
 
+        //log successful initialisation
         LOGGER.info("Bot Initialised");
 
     }
 
     private static void getRoles(){
         roles = new LinkedHashMap<>();
-        List<Guild> guilds = jdaBot.getGuilds();
-        for(Guild guild : guilds){
-            guild.getRoles().forEach(role -> roles.put(role.getId(), role));
-        }
+        jdaBot.getGuilds().forEach(guild -> guild.getRoles().forEach(role -> roles.put(role.getId(), role)));
+
     }
 
 }
