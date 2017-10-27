40a958731e53142823902aa77ed8ed7235bdab35
Ali <sycanpvp@gmail.com>
2017-10-27 21:54:19 +0100

    Removed Useless Imports

--- src/main/java/net/legenda/DiscordBot/managers/MessageManager.java
+++ src/main/java/net/legenda/DiscordBot/managers/MessageManager.java
@@ -3,10 +3,6 @@ package net.legenda.DiscordBot.managers;
 import net.legenda.DiscordBot.command.Command;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
-import net.legenda.DiscordBot.exceptions.IllegalCommandAccessException;
-import net.legenda.DiscordBot.exceptions.InvalidCommandArgumentException;
-import net.legenda.DiscordBot.exceptions.InvalidCommandException;
-import net.legenda.DiscordBot.exceptions.InvalidCommandStateException;
 
 public class MessageManager extends ListenerAdapter {
 
