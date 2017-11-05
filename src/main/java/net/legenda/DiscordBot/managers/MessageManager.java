66e6c6ce586ec2759d8d98979084fa7a4caad897
Ali <sycanpvp@gmail.com>
2017-11-05 11:08:26 +0000

    Added Log On Exception

--- src/main/java/net/legenda/DiscordBot/managers/MessageManager.java
+++ src/main/java/net/legenda/DiscordBot/managers/MessageManager.java
@@ -1,5 +1,6 @@
 package net.legenda.DiscordBot.managers;
 
+import net.legenda.DiscordBot.Main;
 import net.legenda.DiscordBot.command.Command;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -11,6 +12,7 @@ public class MessageManager extends ListenerAdapter {
         try {
             CommandManager.execute(event);
         } catch(Exception e){
+            Main.LOGGER.warn(e.getLocalizedMessage());
             Command.sendErrorMessage(e.getLocalizedMessage(), event.getTextChannel(),false);
         }
     }
