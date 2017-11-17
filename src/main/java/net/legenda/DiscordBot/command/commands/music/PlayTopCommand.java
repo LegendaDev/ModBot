8cb77ce7bfa58e77a1ba056b8dafcf1e05280ee2
Ali <sycanpvp@gmail.com>
2017-11-17 21:18:05 +0000

    Added .PlayTop Command

---
+++ src/main/java/net/legenda/DiscordBot/command/commands/music/PlayTopCommand.java
@@ -0,0 +1,30 @@
+package net.legenda.DiscordBot.command.commands.music;
+
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.legenda.DiscordBot.Main;
+import net.legenda.DiscordBot.command.Command;
+import net.legenda.DiscordBot.exceptions.InvalidCommandArgumentException;
+
+import java.util.Arrays;
+
+
+@Command.cmdInfo(name = "PlayTop", description = "Adds a song to the queue", type = Command.Type.Music)
+public class PlayTopCommand extends Command {
+
+    @Override
+    public void execute(String[] args, MessageReceivedEvent event) {
+        if(args.length < 1){
+            throw new InvalidCommandArgumentException("Usage: .Play (Link/Search Query)");
+        }
+
+        String input = String.join(" ", Arrays.copyOfRange(args, 0, args.length));
+
+        if(!(input.startsWith("https://") || input.startsWith("http://"))){
+            input = "ytsearch:" + input;
+        }
+
+        Main.INSTANCE.musicUtils.newTrack(input, event.getMember(), event.getMessage(), true);
+
+    }
+
+}
